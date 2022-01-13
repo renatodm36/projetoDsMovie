739fe6b8a9ef725d2f620a237413f93eb8ccceb3
Renato Duarte <renatodm36@gmail.com>
2022-01-13 19:20:47 -0300

    Navgation button

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './style.css'
 
 function Form() {
@@ -12,7 +13,7 @@ function Form() {
 
     return (
         <div className="dsmovie-form-container">
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <form className="dsmovie-form">
@@ -34,7 +35,9 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     );
